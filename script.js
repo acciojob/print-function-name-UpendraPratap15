14f34e4d825a41362bd40df5a84fbab87773087c
Upendra Pratap Sharma <140492452+UpendraPratap15@users.noreply.github.com>
2026-01-05 19:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function functionName() {
+    alert(arguments.callee.name);
+}
+
+function AccioJob() {
+    alert(arguments.callee.name);
+}
+
+// You can call the functions like this:
+// functionName();
+// AccioJob();
